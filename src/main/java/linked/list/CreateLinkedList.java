e4bc50fdc3e7951ffc4b148c45a65ce2f9ca8ebf
Aman Raj <aman.raj@metlife.com>
2023-02-22 16:13:29 +0530

    LinkedList methods added

--- src/main/java/linked/list/CreateLinkedList.java
+++ src/main/java/linked/list/CreateLinkedList.java
@@ -51,6 +51,80 @@ public class CreateLinkedList {
         return temp;
     }
 
+    public boolean prepend(int value){
+        Node new_node = new Node(value);
+        if (this.length == 0){
+            this.head = new_node;
+            this.tail = new_node;
+            this.length += 1;
+            return true;
+        }
+        new_node.next = this.head;
+        this.head = new_node;
+        this.length += 1;
+        return true;
+    }
+
+    public Node popFirst(){
+        if (this.length == 0){
+            return null;
+        }
+        Node temp = this.head;
+        if (this.length == 1){
+            this.head = null;
+            this.tail = null;
+            this.length -= 1;
+            return temp;
+        }else{
+            this.head = this.head.next;
+            temp.next = null;
+            this.length -= 1;
+            return temp;
+        }
+    }
+
+    public Node get(int index){
+        if (index >= this.length || index < 0 ){
+            return null;
+        }
+        if (index == 0){
+            return this.head;
+        }
+        Node temp = this.head;
+        for (int i = 0; i < index; i++){
+            temp = temp.next;
+        }
+        return temp;
+
+    }
+
+    public boolean setValue(int index, int value){
+        Node temp = get(index);
+        if (temp != null){
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value){
+        if (index > this.length || index < 0) return false;
+        if (index == 0) return this.prepend(value);
+        if (index == this.length) return this.append(value);
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        this.length += 1;
+        return true;
+    }
+    /*
+    public Node remove(int index){
+        if(index >= this.length || index < 0){
+
+        }
+    }*/
+
     public void print_values(){
         if(this.length == 0){
             System.out.println("Empty List");
--- src/main/java/linked/list/TestList.java
+++ src/main/java/linked/list/TestList.java
@@ -8,7 +8,25 @@ public class TestList {
         linkedList.append(2);
         linkedList.append(3);
         linkedList.print_values();
+        System.out.println("----------");
         System.out.println(linkedList.pop().value);
+        System.out.println("----------");
+        linkedList.prepend(3);
+        linkedList.prepend(4);
+        linkedList.print_values();
+        System.out.println("----------");
+        System.out.println(linkedList.popFirst().value);
+        System.out.println("----------");
+        linkedList.print_values();
+        System.out.println("----------");
+        System.out.println(linkedList.get(2).value);
+        System.out.println("----------");
+        System.out.println(linkedList.setValue(0,1));
+        System.out.println(linkedList.setValue(1,2));
+        System.out.println(linkedList.setValue(2,3));
+        linkedList.print_values();
+        System.out.println("----------");
+        System.out.println(linkedList.insert(1, 7));
         linkedList.print_values();
     }
 }
